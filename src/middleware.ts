4e4675eba741951cb338f851eaf0d8e941e3ebeb
Augusto Dias <gutodiasdev@gmail.com>
2024-05-30 07:45:57 -0300

    refact(middleware): refact middleware to add protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,18 @@
-export function middleware() {
+import { cookies } from "next/headers";
+import { NextResponse, type NextRequest } from "next/server";
 
+const publicRoutes = [
+  "/",
+  "/login",
+  "/cadastre-se"
+]
+
+export function middleware(request: NextRequest) {
+  const cookieStore = cookies();
+  const token = cookieStore.get("__postgate.session");
+  if (!publicRoutes.includes(request.nextUrl.pathname) && !token) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
 }
 
 export const config = {
