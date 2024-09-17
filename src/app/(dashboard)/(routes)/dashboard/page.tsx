7e6d6450f6bed84663675d45a8905be8f6d0f565
Augusto Dias <gutodiasdev@gmail.com>
2024-09-17 10:51:02 -0300

    hotfix: change to NEXT_PUBLIC

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -11,7 +11,7 @@ import {
 export default async function Page() {
   const cookiesStore = cookies();
   const authorization = cookiesStore.get("__postgate.session")
-  const { data } = await fetch(process.env.API_URL + "/dashboard", {
+  const { data } = await fetch(process.env.NEXT_PUBLIC_API_URL + "/dashboard", {
     headers: {
       Authorization: `Bearer ${authorization?.value}`,
     }
