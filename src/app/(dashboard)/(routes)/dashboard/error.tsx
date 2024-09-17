c710f3f29e9e9e3d2ab43e9ec1021334c7fe0a90
Augusto Dias <gutodiasdev@gmail.com>
2024-09-17 11:43:39 -0300

    hotfix: adjust word

--- src/app/(dashboard)/(routes)/dashboard/error.tsx
+++ src/app/(dashboard)/(routes)/dashboard/error.tsx
@@ -11,7 +11,7 @@ export default function Custom500() {
           Esta página está em manutenção.
         </span>
         <span className="">
-          Estamos trabalhando para tudo seja normalizado.
+          Estamos trabalhando para que tudo seja normalizado.
         </span>
       </div>
     </section>
